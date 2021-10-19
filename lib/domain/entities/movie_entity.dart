a0596b4f7227e44c9e735d4f211fe745428d8b32
Ribesh Basnet <badribesh@gmail.com>
2021-10-19 10:22:04 +0545

    created movie entity.

---
+++ lib/domain/entities/movie_entity.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class MovieEntity extends Equatable {
+  final String? posterPath;
+  final int id;
+  final String? backdropPath;
+  final String? title;
+  final String? voteAverage;
+  final String? releaseDate;
+  final String? overview;
+
+  const MovieEntity(
+      {this.posterPath,
+      required this.id,
+      this.backdropPath,
+      this.title,
+      this.voteAverage,
+      this.releaseDate,
+      this.overview});
+
+  @override
+  List<Object?> get props => [id, title];
+
+  @override
+  bool get stringify => true;
+}
