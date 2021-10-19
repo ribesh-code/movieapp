a66ed057e9feaa8cfc856f6bb96d66f2386ecfc1
Ribesh Basnet <badribesh@gmail.com>
2021-10-19 10:03:37 +0545

    created api const.

---
+++ lib/data/core/api_constanst.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = 'c0749a59052602dd61d72508b7247efc';
+  static const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+}
