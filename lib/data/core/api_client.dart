c93aba84dc348705c4e917e590dd1a929c041a66
Ribesh Basnet <badribesh@gmail.com>
2021-10-19 14:09:52 +0545

    created base class for http client.

---
+++ lib/data/core/api_client.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:movieapp/data/core/api_constanst.dart';
+
+class ApiClient {
+  final Client _client;
+
+  ApiClient(this._client);
+
+  dynamic get(String path) async {
+    final response = await _client.get(
+        Uri.parse(
+          '${ApiConstants.BASE_URL}$path?api_key=${ApiConstants.API_KEY}',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        });
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+}
