abac48c8496fb61b78825a15bb982ad2f0c97948
Ribesh Basnet <badribesh@gmail.com>
2021-10-22 00:32:15 +0545

    created bloc for movie carousel.

---
+++ lib/presentation/states/movie_carousel/movie_carousel_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:movieapp/domain/entities/movie_entity.dart';
+import 'package:movieapp/domain/entities/no_params.dart';
+import 'package:movieapp/domain/usecases/get_trending.dart';
+
+part 'movie_carousel_event.dart';
+part 'movie_carousel_state.dart';
+
+class MovieCarouselBloc extends Bloc<MovieCarouselEvent, MovieCarouselState> {
+  final GetTrending getTrending;
+
+  MovieCarouselBloc({required this.getTrending})
+      : super(MovieCarouselInitial());
+
+  @override
+  Stream<MovieCarouselState> mapEventToState(
+    MovieCarouselEvent event,
+  ) async* {
+    if (event is CarouselLoadEvent) {
+      final moviesEither = await getTrending(NoParams());
+      yield moviesEither.fold(
+        (l) => const MovieCarouselError(
+          message: 'Something went wrong',
+        ),
+        (r) => MovieCarouselLoaded(movies: r),
+      );
+    }
+  }
+}
