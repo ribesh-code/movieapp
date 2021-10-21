f99e67966899179e17ff9508fba3616a16913c78
Ribesh Basnet <badribesh@gmail.com>
2021-10-21 23:59:53 +0545

    code clean up.

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,15 @@
 // ignore_for_file: avoid_print
 
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:movieapp/domain/entities/app_error.dart';
-import 'package:movieapp/domain/entities/movie_entity.dart';
-import 'package:movieapp/domain/entities/no_params.dart';
-import 'package:movieapp/domain/usecases/get_trending.dart';
+import 'package:flutter/services.dart';
+import 'package:movieapp/presentation/movie_app.dart';
 import 'package:pedantic/pedantic.dart';
 import 'di/get_it.dart' as getIt;
 
-void main() async {
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  unawaited(
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]));
   unawaited(getIt.init());
-  GetTrending getTrending = getIt.getInstance<GetTrending>();
-  final Either<AppError, List<MovieEntity>> eitherResponse =
-      await getTrending(NoParams());
-  eitherResponse.fold((l) {
-    print('left has been called');
-    print(l);
-  }, (r) {
-    print('right called');
-    print(r);
-  });
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
-  }
+  runApp(const MovieApp());
 }
