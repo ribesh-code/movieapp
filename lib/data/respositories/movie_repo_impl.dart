720d46a264fe7aabf5751dc2feb4d14237a06fe0
Ribesh Basnet <badribesh@gmail.com>
2021-10-21 23:36:16 +0545

    added more methods in movies repos and impl them.

--- lib/data/respositories/movie_repo_impl.dart
+++ lib/data/respositories/movie_repo_impl.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:movieapp/data/data_source/remote/movie_remote_data_source.dart';
 import 'package:movieapp/data/models/movie_model.dart';
+import 'package:movieapp/domain/entities/app_error.dart';
 import 'package:movieapp/domain/entities/movie_entity.dart';
 import 'package:movieapp/domain/repositories/movie_repo.dart';
 
@@ -8,12 +10,50 @@ class MovieRepoImpl extends MovieRepo {
   MovieRepoImpl({required this.movieRemoteDataSource});
 
   @override
-  Future<List<MovieModel>> getTrending() async {
+  Future<Either<AppError, List<MovieModel>>> getTrending() async {
     try {
-      final movies = await movieRemoteDataSource.getTrending();
-      return movies;
+      final trendingMovies = await movieRemoteDataSource.getTrending();
+      return Right(trendingMovies);
     } on Exception {
-      throw Exception('cannot fecth');
+      return const Left(
+        AppError(message: 'Cannot Get Trending Movies'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<AppError, List<MovieEntity>>> getCommingSoon() async {
+    try {
+      final commingSoon = await movieRemoteDataSource.getCommingSoon();
+      return Right(commingSoon);
+    } on Exception {
+      return const Left(
+        AppError(
+          message: ('Cannot Get Comming Soon Movies'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<AppError, List<MovieEntity>>> getPlayingNow() async {
+    try {
+      final playingNow = await movieRemoteDataSource.getPlayingNow();
+      return Right(playingNow);
+    } on Exception {
+      return const Left(AppError(message: 'Cannot Get Playing Now Movies'));
+    }
+  }
+
+  @override
+  Future<Either<AppError, List<MovieEntity>>> getPopular() async {
+    try {
+      final popularMovies = await movieRemoteDataSource.getPopular();
+      return Right(popularMovies);
+    } on Exception {
+      return const Left(
+        AppError(message: 'Cannot Get Popular Movies.'),
+      );
     }
   }
 }
