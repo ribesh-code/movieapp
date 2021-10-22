75af6178228720a43c67ac71a8c058a576d652b5
Ribesh Basnet <badribesh@gmail.com>
2021-10-22 10:21:18 +0545

    created movie app bar.

---
+++ lib/presentation/widgets/movie_app_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:movieapp/common/constants/size_constants.dart';
+import 'package:movieapp/common/screen_utils/screen_utils.dart';
+import 'package:movieapp/presentation/widgets/logo.dart';
+import '../../common/extension/size_extension.dart';
+
+class MovieAppBar extends StatelessWidget {
+  const MovieAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: ScreenUtil.statusBarHeight + Sizes.dimen_4.h.toDouble(),
+        left: Sizes.dimen_16.w.toDouble(),
+        right: Sizes.dimen_16.w.toDouble(),
+      ),
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/svgs/menu.svg',
+              height: Sizes.dimen_12.h.toDouble(),
+            ),
+          ),
+          const Expanded(
+            child: Logo(height: Sizes.dimen_14),
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+                size: Sizes.dimen_12.h.toDouble(),
+              ))
+        ],
+      ),
+    );
+  }
+}
