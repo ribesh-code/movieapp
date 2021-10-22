b6045d429bb84c7d5458193d3e48cd057fcdc6d8
Ribesh Basnet <badribesh@gmail.com>
2021-10-22 10:23:37 +0545

    created movie backdrop blocs.

--- lib/presentation/states/movie_carousel/movie_carousel_bloc.dart
+++ lib/presentation/states/movie_carousel/movie_carousel_bloc.dart
@@ -3,14 +3,17 @@ import 'package:equatable/equatable.dart';
 import 'package:movieapp/domain/entities/movie_entity.dart';
 import 'package:movieapp/domain/entities/no_params.dart';
 import 'package:movieapp/domain/usecases/get_trending.dart';
+import 'package:movieapp/presentation/states/movie_backdrop/movie_backdrop_bloc.dart';
 
 part 'movie_carousel_event.dart';
 part 'movie_carousel_state.dart';
 
 class MovieCarouselBloc extends Bloc<MovieCarouselEvent, MovieCarouselState> {
   final GetTrending getTrending;
+  final MovieBackdropBloc movieBackdropBloc;
 
-  MovieCarouselBloc({required this.getTrending})
+  MovieCarouselBloc(
+      {required this.getTrending, required this.movieBackdropBloc})
       : super(MovieCarouselInitial());
 
   @override
@@ -20,11 +23,13 @@ class MovieCarouselBloc extends Bloc<MovieCarouselEvent, MovieCarouselState> {
     if (event is CarouselLoadEvent) {
       final moviesEither = await getTrending(NoParams());
       yield moviesEither.fold(
-        (l) => const MovieCarouselError(
-          message: 'Something went wrong',
-        ),
-        (r) => MovieCarouselLoaded(movies: r),
-      );
+          (l) => const MovieCarouselError(
+                message: 'Something went wrong',
+              ), (r) {
+        movieBackdropBloc
+            .add(MovieBackDropChangedEvent(movie: r[event.defaultIndex]));
+        return MovieCarouselLoaded(movies: r, defaultIndex: event.defaultIndex);
+      });
     }
   }
 }
